3e7a4d64eac21f9e5cd62a2631817d85a16545ab
PabloChamba <136023175+PabloChamba@users.noreply.github.com>
2023-06-16 01:39:57 +0000

    Implementacion de modos

---
+++ programa.py
@@ -0,0 +1,9 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print(sumar(2,3))
+print(restar(4,2))
+print(multiplicar(5,2))
+print(dividir(4,2))
+
+print("Distancia:", distancia(1,2,4,5))
